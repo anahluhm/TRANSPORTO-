ee171f065dff109f33a7a0e696bbf8cffea2b12c
paula argôlo <153379216+panyfmc@users.noreply.github.com>
2024-11-26 00:51:36 -0300

    :tada: init: primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+modalBackground = document.getElementsByClassName("modal-background")[0];
+
+
+function openModal() {
+  modalBackground.style.display = "block";
+}
+
+function closeModel() {
+  modalBackground.style.display = "none";
+}
